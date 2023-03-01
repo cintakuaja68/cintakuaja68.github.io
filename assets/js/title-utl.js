6c155f2aaab1b79d10e542f5df270fccd594b1f8
cintakuaja68 <122879534+cintakuaja68@users.noreply.github.com>
2023-03-01 18:04:42 +0700

    Update title-utl.js

--- assets/js/title-utl.js
+++ assets/js/title-utl.js
@@ -1,37 +1,44 @@
-function getQueryStringParam(param) {
-  var url = window.location.toString();
-  url.match(/\?(.+)$/);
-  var params = RegExp.$1;
-  params = params.split("&");
-  var queryStringList = {};
-  for(var i = 0; i < params.length; i++) {
-    var tmp = params[i].split("-");
-    queryStringList[tmp[0]] = unescape(tmp[1]);
+
+
+  function getQueryStringParam(param) {
+    var url = window.location.toString();
+    url.match(/\?(.+)$/);
+    var params = RegExp.$1;
+    params = params.split("&");
+    var queryStringList = {};
+    for (var i = 0; i < params.length; i++) {
+      var tmp = params[i].split("-");
+      queryStringList[tmp[0]] = unescape(tmp[1]);
+    }
+    return queryStringList[param];
   }
-  return queryStringList[param];
-}
 
+  if (getQueryStringParam("season")) {
+    var season = getQueryStringParam("season");
+    document.getElementById("episode-title").innerHTML = "Season " + season;
 
+    if (getQueryStringParam("episode")) {
+      var episode = getQueryStringParam("episode");
+      document.getElementById("episode-title").innerHTML +=
+        " - Episode " + episode + " | Full Episode";
+      document.getElementById("episode2-title").innerHTML =
+        "Season " + season + " - Episode " + episode;
+      document.getElementById("episode3-title").innerHTML =
+        " Episode " + episode;
+    } else {
+      document.getElementById("episode2-title").innerHTML =
+        "Season " + season + " - Full Episode";
+      document.getElementById("episode3-title").innerHTML = "";
+    }
+  } else if (getQueryStringParam("episode")) {
+    var episode = getQueryStringParam("episode");
+    document.getElementById("episode-title").innerHTML =
+      "Episode " + episode + " | Full Episode";
+    document.getElementById("episode2-title").innerHTML = "Episode " + episode;
+    document.getElementById("episode3-title").innerHTML = " Episode " + episode;
+  } else {
+    document.getElementById("episode-title").innerHTML = "";
+    document.getElementById("episode2-title").innerHTML = "";
+    document.getElementById("episode3-title").innerHTML = "";
+  }
 
-if (getQueryStringParam("episode")) {
-  var episode = getQueryStringParam("episode");
-  document.getElementById("episode-title").innerHTML = " episode " + episode + " | Full Episode";
-} else {
-  document.getElementById("episode-title").innerHTML = "";
-}
-
-
-if (getQueryStringParam("episode")) {
-  var episode = getQueryStringParam("episode");
-  document.getElementById("episode2-title").innerHTML = " episode " + episode + "";
-} else {
-  document.getElementById("episode2-title").innerHTML = "";
-}
-
-
-if (getQueryStringParam("episode")) {
-  var episode = getQueryStringParam("episode");
-  document.getElementById("episode3-title").innerHTML = " episode " + episode + "";
-} else {
-  document.getElementById("episode3-title").innerHTML = "";
-}
